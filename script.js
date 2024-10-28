ff60387d24a903185af1ea666460f126d492699b
wan180297 <wan180297@gmail.com>
2024-10-27 23:30:06 -0300

    sorteio com js

---
+++ script.js
@@ -0,0 +1,11 @@
+const drawNumber = document.querySelector("button");
+
+function generateNumber() {
+    const min = Math.ceil(document.querySelector(".input-min").value);
+    const max = Math.floor(document.querySelector(".input-max").value);
+
+    const result = Math.floor(Math.random() * ( max - min + 1 ) ) + min;
+    alert(result);
+};
+
+drawNumber.addEventListener("click", generateNumber);
